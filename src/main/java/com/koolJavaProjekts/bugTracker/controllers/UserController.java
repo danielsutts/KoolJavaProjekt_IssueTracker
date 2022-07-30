16da8e1901c2eb0f5da015825e6c737fe524603b
l1stochekkk <rubalkinmakx@gmail.com>
2022-07-30 23:21:47 +0300

    added UserController

---
+++ src/main/java/com/koolJavaProjekts/bugTracker/controllers/UserController.java
@@ -0,0 +1,29 @@
+package com.koolJavaProjekts.bugTracker.controllers;
+
+import com.koolJavaProjekts.bugTracker.models.User;
+import com.koolJavaProjekts.bugTracker.repositories.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    UserRepository userRepository;
+
+    @GetMapping("/user/all")
+    public List<User> getAll(){
+        return userRepository.findAll();
+    }
+
+    @PostMapping("/user/add")
+    public void addUser(String name, String surname, String nickname, String team, String email, String password, String image, long id){
+        userRepository.save(new User(name, surname, nickname, team, email, password, image, id));
+    }
+
+
+
+
+}
