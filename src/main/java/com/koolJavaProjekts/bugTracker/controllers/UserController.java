01223f6da07fbb44fd500a144e6f7ed207b934f9
l1stochekkk <rubalkinmakx@gmail.com>
2022-07-30 23:53:31 +0300

    Updated UserController (Mappings: /update, /delete, /add, /all)

--- src/main/java/com/koolJavaProjekts/bugTracker/controllers/UserController.java
+++ src/main/java/com/koolJavaProjekts/bugTracker/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.koolJavaProjekts.bugTracker.controllers;
 
 import com.koolJavaProjekts.bugTracker.models.User;
 import com.koolJavaProjekts.bugTracker.repositories.UserRepository;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,11 +20,32 @@ public class UserController {
     }
 
     @PostMapping("/user/add")
-    public void addUser(String name, String surname, String nickname, String team, String email, String password, String image, long id){
-        userRepository.save(new User(name, surname, nickname, team, email, password, image, id));
+    public void addUser(User user){
+        userRepository.save(user);
     }
 
+    @PostMapping("/user/update")
+    public void updateUser(User newUser){
+        for (User user : userRepository.findAll()) {
+            if (user.getId() == newUser.getId()) {
+                user.setName(newUser.getName());
+                user.setSurname(newUser.getSurname());
+                user.setEmail(newUser.getEmail());
+                user.setNickname(newUser.getNickname());
+                user.setPassword(newUser.getPassword());
+                user.setImage(newUser.getImage());
+                user.setTeam(newUser.getTeam());
+            }
+        }
+    }
 
-
+    @DeleteMapping("/user/delete")
+    public void deleteUser(User deleteUser){
+        for (User user : userRepository.findAll()) {
+            if (user.getId() == deleteUser.getId()){
+                userRepository.delete(user);
+            }
+        }
+    }
 
 }
