d81ea69210e921e0e621c171c69f38cf26613e36
danielsutts <daniel@iamsutts.tech>
2022-07-31 00:28:11 +0300

    Issue Annotations

--- src/main/java/com/koolJavaProjekts/bugTracker/models/Issue.java
+++ src/main/java/com/koolJavaProjekts/bugTracker/models/Issue.java
@@ -1,30 +1,58 @@
 package com.koolJavaProjekts.bugTracker.models;
 
-public class Issue {
+import javax.persistence.*;
 
+@Entity(name = "issues")
+
+public class Issue {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private final long issueId;
+    @Column
     private final String priority;
+    @Column
     private final String issueName;
+    @Column
     private final String description;
+    @Column
     private final String attachment;
+    @Column
     private final Integer buildVersion;
+    @Column
     private final String requester;
+    @Column
     private final String assignee;
+    @Column
     private final Integer dateOpened;
+    @Column
     private final Integer dateUpdated;
+    @Column
     private final Integer dateDue;
+    @Column
     private final Integer buildPoints;
+    @Column
     private final Integer storyPoints;
+    @Column
     private final Integer mrr;
+    @Column
     private final String project;
+    @Column
     private final long childId;
+    @Column
     private final long parentId;
+    @Column
     private final String summary;
+    @Column
     private final String branchName;
+    @Column
     private final String se;
+    @Column
     private final String qe;
+    @Column
     private final String teamLead;
+    @Column
     private final String issue;
+
 
     public Issue(long issueId, String priority, String issueName, String description, String attachment, Integer buildVersion, String requester, String assignee, Integer dateOpened, Integer dateUpdated, Integer dateDue, Integer buildPoints, Integer storyPoints, Integer mrr, String project, long childId, long parentId, String summary, String branchName, String se, String qe, String teamLead, String issue) {
         this.issueId = issueId;
@@ -50,6 +78,32 @@ public class Issue {
         this.qe = qe;
         this.teamLead = teamLead;
         this.issue = issue;
+    }
+
+    public Issue() {
+        this.issueId = -1;
+        this.priority = "";
+        this.issueName = "";
+        this.description = "";
+        this.attachment = "";
+        this.buildVersion = -1;
+        this.requester = "";
+        this.assignee = "";
+        this.dateOpened = -1;
+        this.dateUpdated = -1;
+        this.dateDue = -1;
+        this.buildPoints = -1;
+        this.storyPoints = -1;
+        this.mrr = -1;
+        this.project = "";
+        this.childId = -1;
+        this.parentId = -1;
+        this.summary = "";
+        this.branchName = "";
+        this.se = "";
+        this.qe = "";
+        this.teamLead = "";
+        this.issue = "";
     }
 
 
